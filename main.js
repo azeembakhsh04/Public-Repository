39e4d6b0caa2f60c6fa696f96c3f35fa81f18bb1
azeembakhsh04 <96291603+azeembakhsh04@users.noreply.github.com>
2023-12-11 11:57:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+export function owner() {
+   if(confirm('Made and Design by Azeem Bakhsh \n visit our youtube channel')) {
+      window.location.href = 'https://www.youtube.com/c/@azeemtech';
+   }
+}
